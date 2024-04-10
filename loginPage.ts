094ba94a14e04afb873eb0ab8f6c2492ff8814fe
abhishekkassal <92214620+abhishekkassal@users.noreply.github.com>
2024-04-10 13:13:47 +0530

    Add files via upload

---
+++ loginPage.ts
@@ -0,0 +1,19 @@
+import { Page } from "@playwright/test"
+export default class LoginPage {
+
+    constructor(public page: Page) { }
+
+    async enterEmail(emailaddress: string) {
+        await this.page.locator("#input[name='email']")
+            .type(emailaddress);
+    }
+
+    async enterLoginPassword(password: string) {
+        await this.page.locator("#input[name='password']")
+            .type(password);
+    }
+
+    async clickLoginBtn() {
+        await this.page.click("input[vaue='Login']");
+    }
+}
